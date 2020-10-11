491a7e7f62a302ab8a8c0600a2dd05285750ba79
guangyongluo(罗葳) <llooww@mail.ustc.edu.cn>
2020-10-11 09:54:53 +0800

    DesignPattern(com.vilin.principle): prototype design pattern .

---
+++ design-pattern/src/main/java/com/vilin/prototype/deepclone/DeepCloneableTarget.java
@@ -0,0 +1,27 @@
+package com.vilin.prototype.deepclone;
+
+import java.io.Serializable;
+
+public class DeepCloneableTarget implements Serializable, Cloneable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String cloneName;
+
+	private String cloneClass;
+
+	//构造器
+	public DeepCloneableTarget(String cloneName, String cloneClass) {
+		this.cloneName = cloneName;
+		this.cloneClass = cloneClass;
+	}
+
+	//因为该类的属性都是String，因此使用默认的clone方法即可
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+}
