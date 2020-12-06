f6b014fc9743cb5027adc02274db8f3948c46e10
guangyongluo(罗葳) <llooww@mail.ustc.edu.cn>
2020-12-06 18:19:28 +0800

    DesignPattern(com.vilin.principle): template design pattern .

---
+++ design-pattern/src/main/java/com/vilin/template/PeanutSoyaMilk.java
@@ -0,0 +1,11 @@
+package com.vilin.template;
+
+public class PeanutSoyaMilk extends SoyaMilk {
+
+	@Override
+	void addCondiments() {
+		// TODO Auto-generated method stub
+		System.out.println("第二部：加入上好的花生");
+	}
+
+}
---
+++ design-pattern/src/main/java/com/vilin/template/RedBeanSoyaMilk.java
@@ -0,0 +1,11 @@
+package com.vilin.template;
+
+public class RedBeanSoyaMilk extends SoyaMilk {
+
+	@Override
+	void addCondiments() {
+		// TODO Auto-generated method stub
+		System.out.println("第二步：加入上好的红豆");
+	}
+
+}
