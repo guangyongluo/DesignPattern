b3115265741f318c0ce7b4b2ab53b5ead61e9ba2
guangyongluo(罗葳) <llooww@mail.ustc.edu.cn>
2020-12-12 14:39:53 +0800

    DesignPattern(com.vilin.principle): responsibility chain design pattern .

---
+++ design-pattern/src/main/java/com/vilin/responsibilitychain/Approver.java
@@ -0,0 +1,21 @@
+package com.vilin.responsibilitychain;
+
+public abstract class Approver {
+
+	Approver approver;  //下一个处理者
+	String name; // 名字
+	
+	public Approver(String name) {
+		// TODO Auto-generated constructor stub
+		this.name = name;
+	}
+
+	//下一个处理者
+	public void setApprover(Approver approver) {
+		this.approver = approver;
+	}
+	
+	//处理审批请求的方法，得到一个请求，处理是子类完成的，因此该方法做成抽象的
+	public abstract void processRequest(PurchaseRequest purchaseRequest);
+	
+}
